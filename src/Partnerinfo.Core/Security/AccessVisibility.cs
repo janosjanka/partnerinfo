8be64a9a7b47ae6da97e4b02042bc212bb741972
János Janka <jankajanos@msn.com>
2016-09-27 07:56:45 +0200

    Security

---
+++ src/Partnerinfo.Core/Security/AccessVisibility.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) János Janka. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Partnerinfo.Security
+{
+    /// <summary>
+    /// Represents an access visibility value indicating whether a resource is available on the Web.
+    /// </summary>
+    public enum AccessVisibility : byte
+    {
+        /// <summary>
+        /// Indicates that the visibility is not being used.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Indicates a public resource on the Internet.
+        /// </summary>
+        Public = 10,
+
+        /// <summary>
+        /// Indicates a public resource which is only readable by a link and not indexed by search engines.
+        /// </summary>
+        AnyoneWithLink = 20,
+
+        /// <summary>
+        /// Indicates a protected resource which is only readable by a link shipped with a contact identifier.
+        /// </summary>
+        ContactWithLink = 30,
+
+        /// <summary>
+        /// Indicates a private resource which is only readable by system users.
+        /// </summary>
+        Private = 40
+    }
+}
---
+++ src/Partnerinfo.Core/Security/SecurityResult.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) János Janka. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Partnerinfo.Security
+{
+    /// <summary>
+    /// Represents the result of security operations.
+    /// </summary>
+    public sealed class SecurityResult
+    {
+        /// <summary>
+        /// A <see cref="SecurityResult" /> to refuse to take any further action. It corresponds to HTTP 403 Forbidden.
+        /// </summary>
+        public static readonly SecurityResult AccessDenied = new SecurityResult(false, AccessVisibility.Unknown);
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance has permission.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if the access attempt was successful; otherwise, <c>false</c>.
+        /// </value>
+        public bool AccessGranted { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the resource can be available on the Web.
+        /// </summary>
+        /// <value>
+        /// The visibility for an ACE (Access Control Entry).
+        /// </value>
+        public AccessVisibility Visibility { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SecurityResult" /> class.
+        /// </summary>
+        /// <param name="accessGranted"><c>true</c> if the access attempt was successful; otherwise, <c>false</c>.</param>
+        /// <param name="visibility">The visibility.</param>
+        public SecurityResult(bool accessGranted, AccessVisibility visibility)
+        {
+            AccessGranted = accessGranted;
+            Visibility = visibility;
+        }
+    }
+}
