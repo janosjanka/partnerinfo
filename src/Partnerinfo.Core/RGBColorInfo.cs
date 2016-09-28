733038cb1051eeb2144dad70c040b6eeb13e5de4
János Janka <jankajanos@msn.com>
2016-09-28 22:50:28 +0200

    Fix

--- src/Partnerinfo.Core/RGBColorInfo.cs
+++ src/Partnerinfo.Core/RGBColorInfo.cs
@@ -111,14 +111,6 @@ namespace Partnerinfo
         }
 
         /// <summary>
-        /// Returns a hash code for this instance.
-        /// </summary>
-        /// <returns>
-        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
-        /// </returns>
-        public sealed override int GetHashCode() => RGB.GetHashCode(); // Hopefully, the compiler will optimize it out.
-
-        /// <summary>
         /// Returns a <see cref="string" /> that represents this instance.
         /// </summary>
         /// <returns>
