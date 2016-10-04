22783d5d74e489a4ddaa2569013dbee6585a07ca
János Janka <jankajanos@msn.com>
2016-10-04 13:03:23 +0200

    Declares interfaces for XHR results

--- src/Partnerinfo.Web.Site/ClientApp/src/@api/types.ts
+++ src/Partnerinfo.Web.Site/ClientApp/src/@api/types.ts
@@ -4,6 +4,11 @@
   * Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
   */
 
-export interface OperationResult {
-
+export interface ListResult<T> {
+    /** The strongly typed list of results to return. */
+    data: ArrayLike<T>;
+    /** The link to the previous page of this list. */
+    prevLink: string;
+    /** The link to the next page of this list. */
+    nextLink: string;
 }
