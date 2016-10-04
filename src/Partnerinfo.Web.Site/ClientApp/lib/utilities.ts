ede1e31549c3f17ecd5d1967b8ccf4fd2828525b
János Janka <jankajanos@msn.com>
2016-10-04 13:46:26 +0200

    Add utilities

---
+++ src/Partnerinfo.Web.Site/ClientApp/lib/utilities.ts
@@ -0,0 +1,30 @@
+﻿/**
+  * @license
+  * Copyright (c) János Janka. All rights reserved.
+  * Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+  */
+
+/**
+ * Returns a new function that will execute only once, coalescing multiple sequential calls into a single execution.
+ * @param callback  A callback you want to execute after delay milliseconds.
+ * @param delay     The number of milliseconds (thousandths of a second) that the function call should be delayed by.
+ * @param immediate If true, trigger the function on the leading edge, instead of the trailing.
+ */
+export function debounce(callback: Function, delay: number = 0, immediate: boolean = false) {
+    let timeout: number;
+    return function () {
+        const args = arguments;
+        const later = () => {
+            timeout = null;
+            if (!immediate) {
+                callback.apply(this, args);
+            }
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, delay);
+        if (callNow) {
+            callback.apply(this, args);
+        }
+    };
+}
