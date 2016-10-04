89f795daa0c91e4a59cf2573b8a9614e84ddccb1
János Janka <jankajanos@msn.com>
2016-10-04 15:20:15 +0200

    Actions controller

---
+++ src/Partnerinfo.Web.Api/Actions/Controllers/ActionsController.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) János Janka. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Partnerinfo.Actions.Controllers
+{
+    /// <summary>
+    /// Provides methods that respond to HTTP requests that are made to an ASP.NET MVC Web site.
+    /// </summary>
+    [Route("api/[controller]")]
+    public sealed class ActionsController : Controller
+    {
+    }
+}
