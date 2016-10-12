8c4020356545b60d6fed20404105c2ceaadfd1da
János Janka <jankajanos@msn.com>
2016-10-12 19:11:20 +0200

    Add unit tests to the project

---
+++ test/Partnerinfo.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Partnerinfo.Core.Test")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5115484e-f85c-48d6-8794-06ff9e0c1aeb")]
---
+++ test/Partnerinfo.Core.Test/UriPartNormalizerTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) János Janka. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Partnerinfo
+{
+    public class UriPartNormalizerTests
+    {
+        [Fact]
+        public void ShouldReturnValidUrlWithoutWhiteSpaces()
+        {
+            // Setup
+            var normalizer = new UriPartNormalizer();
+
+            // Act
+            var result = normalizer.Normalize("Janka  János Zoltán   ");
+
+            // Assert
+            Assert.Equal(result, "janka-janos-zoltan");
+        }
+    }
+}
