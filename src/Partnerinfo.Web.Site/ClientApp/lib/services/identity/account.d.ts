414427913e5e15423a8e72687e91eff2bcdf6eda
János Janka <jankajanos@msn.com>
2016-10-18 22:59:57 +0200

    Service class is exported as default class.

---
+++ src/Partnerinfo.Web.Site/ClientApp/lib/services/identity/account.d.ts
@@ -0,0 +1,18 @@
+﻿// Copyright (c) János Janka. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+/** Used to log in a user to the system. **/
+interface LoginOptions {
+    email: string;
+    password: string;
+}
+
+/** Used to register a new user to the system. */
+interface RegisterOptions {
+    email: string;
+    password: string;
+    firstName?: string;
+    lastName?: string;
+    gender?: "male" | "female";
+    birthday?: Date;
+}
--- src/Partnerinfo.Web.Site/ClientApp/lib/services/identity/account.ts
+++ src/Partnerinfo.Web.Site/ClientApp/lib/services/identity/account.ts
@@ -1,29 +1,14 @@
 ﻿// Copyright (c) János Janka. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-import { api } from "../core";
+/// <reference path="account.d.ts" />
+import * as core from "../core";
 
-/** Used to log in a user to the system. **/
-export interface LoginOptions {
-    email: string;
-    password: string;
-}
-
-/** Used to register a new user to the system. */
-export interface RegisterOptions {
-    email: string;
-    password: string;
-    firstName?: string;
-    lastName?: string;
-    gender?: "male" | "female";
-    birthday?: Date;
-}
-
-export class AccountService {
+export default class {
 
     /** Logs In using an authentication provider as a HTTP POST operation. */
-    login(options: LoginOptions): PromiseLike<any> {
-        return api({
+    login(options: LoginOptions): PromiseLike<void> {
+        return core.api<void>({
             path: "account/login",
             method: "post",
             params: options
@@ -31,8 +16,8 @@ export class AccountService {
     }
 
     /** Registers a new account as a HTTP POST operation. */
-    register(options: RegisterOptions): PromiseLike<any> {
-        return api({
+    register(options: RegisterOptions): PromiseLike<void> {
+        return core.api<void>({
             path: "account/register",
             method: "post",
             params: options
@@ -40,8 +25,8 @@ export class AccountService {
     }
 
     /** Unregisters an existing account as a HTTP POST operation. */
-    unregister(options: LoginOptions): PromiseLike<any> {
-        return api({
+    unregister(options: LoginOptions): PromiseLike<void> {
+        return core.api<void>({
             path: "account/unregister",
             method: "post",
             params: options
