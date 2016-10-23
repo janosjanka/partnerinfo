50ffdc95b130cc488d2212822ced2d629b9d0851
János Janka <jankajanos@msn.com>
2016-10-23 16:49:28 +0200

    Karma & config files.

---
+++ src/Partnerinfo.Web.Site/karma.config.js
@@ -0,0 +1,21 @@
+﻿// Copyright (c) János Janka. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+module.exports = function (config) {
+    config.set({
+        files: [
+          { pattern: "test/*_test.js", watched: false },
+          { pattern: "test/**/*_test.js", watched: false }
+        ],
+        preprocessors: {
+            "test/*_test.js": ["webpack"],
+            "test/**/*_test.js": ["webpack"]
+        },
+        webpack: {
+            devtool: "inline-source-map"
+        },
+        webpackMiddleware: {
+            stats: "errors-only"
+        }
+    });
+};
