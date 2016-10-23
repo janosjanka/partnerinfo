33cf2f321bd8c13724cc3b7ceaa745b3d7bd2a92
János Janka <jankajanos@msn.com>
2016-10-23 09:17:43 +0200

    Build CMD files.

--- src/Partnerinfo.Web.Site/build-debug.cmd
+++ src/Partnerinfo.Web.Site/build-debug.cmd
@@ -1,6 +1,14 @@
 @ECHO OFF
 
+ECHO Downloading node packages...
+CALL npm install -optional
+
+ECHO.
+ECHO Building core library scripts (DEBUG) ...
 node "node_modules/webpack/bin/webpack.js" --config "webpack.config.corefx.js"
+
+ECHO.
+ECHO Building application scripts (DEBUG) ...
 node "node_modules/webpack/bin/webpack.js"
 
 PAUSE
