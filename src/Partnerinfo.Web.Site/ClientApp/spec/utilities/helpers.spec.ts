24d152fec8ef90a808a9e9f0658648ed98b6a335
János Janka <jankajanos@msn.com>
2016-10-23 23:45:51 +0200

    Helpers Spec.

--- src/Partnerinfo.Web.Site/ClientApp/spec/utilities/helpers.spec.ts
+++ src/Partnerinfo.Web.Site/ClientApp/spec/utilities/helpers.spec.ts
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) János Janka. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+import * as helpers from "../../src/utilities/helpers";
+
 describe("utilities.helpers", function () {
     it("contains spec with an expectation", function () {
         expect(true).toBe(true);
