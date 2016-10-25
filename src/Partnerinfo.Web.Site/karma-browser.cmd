250ee145726db052edd7fd4e9784b6639fa0e8d2
János Janka <jankajanos@msn.com>
2016-10-25 18:43:33 +0200

    build-spec => karma-browser

--- src/Partnerinfo.Web.Site/karma-browser.cmd
+++ src/Partnerinfo.Web.Site/karma-browser.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-ECHO Partnerinfo - Development Build
+ECHO Partnerinfo - Karma Browser Tests
 ECHO Copyright (c) Janos Janka. All rights reserved.
 ECHO Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
