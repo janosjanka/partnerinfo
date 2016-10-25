5e12a16f7e67dd4e98773c6cfc95fe2e092ec225
János Janka <jankajanos@msn.com>
2016-10-25 11:49:00 +0200

    Config file for shared settings.

--- src/Partnerinfo.Web.Site/webpack.config.js
+++ src/Partnerinfo.Web.Site/webpack.config.js
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 const config = require("./comm.config");
-
 const path = require("path");
 const webpack = require("webpack");
 const WebpackTextPlugin = require("extract-text-webpack-plugin");
