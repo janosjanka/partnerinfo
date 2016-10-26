eb0a42bb9776c09bf9d62367a4c475124a11a5ec
János Janka <jankajanos@msn.com>
2016-10-26 14:22:13 +0200

    JS test config updates.

--- src/Partnerinfo.Web.Site/build-dist.cmd
+++ src/Partnerinfo.Web.Site/build-dist.cmd
@@ -5,20 +5,16 @@ ECHO Copyright (c) Janos Janka. All rights reserved.
 ECHO Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 ECHO.
-ECHO 1/4. Delete the jQuery folder (bug fix)...
+ECHO Preparing ...
 RMDIR "node_modules/jQuery" /S /Q
 
 ECHO.
-ECHO 2/4. Restoring node packages...
-CALL npm install
+ECHO Building ...
+CALL npm run build:dist
 
 ECHO.
-ECHO 3/4. Building core library...
-CALL node "node_modules/webpack/bin/webpack.js" --dist --config "webpack.config.corefx.js"
-
-ECHO.
-ECHO 4/4. Building application...
-CALL node "node_modules/webpack/bin/webpack.js" --dist
+ECHO Running tests ...
+CALL npm run test:single
 
 ECHO.
 PAUSE
