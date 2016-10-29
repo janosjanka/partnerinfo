3dc5eadca3bbf896b84869a3c9360a4ea21b1586
János Janka <jankajanos@msn.com>
2016-10-29 11:06:33 +0200

    New ColorPicker tests.

--- src/Partnerinfo.Web.Site/ClientApp/spec/components/controls/colorpicker.spec.ts
+++ src/Partnerinfo.Web.Site/ClientApp/spec/components/controls/colorpicker.spec.ts
@@ -11,25 +11,40 @@ const testComponentName = "colorpicker";
 describe(`components > controls > ${testComponentName}`, () => {
     beforeEach(() => {
         jasmine.prepareTestNode();
+        jasmine.testNode.innerHTML = `<div data-bind="component: { name: '${testComponentName}', params: $data }"></div>`;
         ko.components.register(testComponentName, colorPicker);
     });
 
-    it("sets the value to the background-color of the clicked item", done => {
-        const testComponentParams = { value: ko.observable<string>() };
-        ko.utils.setHtml(jasmine.testNode, `<div data-bind="component: { name: '${testComponentName}', params: $data }"></div>`);
+    it("should be initialized with an observable value '#ffffff'", done => {
+        const testComponentParams = { value: ko.observable<string>("#ffffff") };
 
         // Since components are loaded asynchronously, it doesn't show up synchronously.
         ko.applyBindings(testComponentParams, jasmine.testNode);
 
-        // It is used internally for Knockout components to maintain asynchronous behavior,
-        // and for scheduling deferred updates for observables.
-        // http://knockoutjs.com/documentation/microtasks.html
+        // It is used internally for components to maintain asynchronous behavior.
+        ko.tasks.schedule(() => {
+            const viewModel = ko.dataFor(jasmine.testNode);
+            expect(viewModel).toBeDefined();
+            expect(viewModel).not.toBeNull();
+            expect(ko.unwrap(viewModel.value)).toBe("#ffffff");
+            done();
+        });
+    });
+
+    it("sets the value to the background-color of the clicked item", done => {
+        const testComponentParams = { value: ko.observable<string>() };
+        jasmine.testNode.innerHTML = `<div data-bind="component: { name: '${testComponentName}', params: $data }"></div>`;
+
+        // Since components are loaded asynchronously, it doesn't show up synchronously.
+        ko.applyBindings(testComponentParams, jasmine.testNode);
+
+        // It is used internally for components to maintain asynchronous behavior.
         ko.tasks.schedule(() => {
             const itemList = jasmine.testNode.querySelectorAll(".ui-colorpicker-item") as NodeListOf<HTMLElement>;
             for (let i = 0; i < itemList.length; ++i) {
                 const item = itemList[i];
                 ko.utils.triggerEvent(item, "click");
-                expect(color.rgbToHex(item.style.backgroundColor)).toEqual(testComponentParams.value());
+                expect(color.rgbToHex(item.style.backgroundColor)).toBe(ko.unwrap(testComponentParams.value));
             }
             done();
         });
